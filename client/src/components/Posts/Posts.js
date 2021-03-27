8437429248ffcd7bfc0016411b4e8c9a583fcc7f
[Dawum Nam] <[dawumnam@gmail.com]>
2021-03-26 20:12:49 -0800

    Form added

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
 function Posts() {
   const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+
+  console.log(posts);
 
   return (
     <>
+      <h2>POSTS</h2>
       <Post />
       <Post />
     </>
--- server/index.js
+++ server/index.js
@@ -1,30 +1,37 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import postRoutes from './routes/posts.js'
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use('/posts', postRoutes)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json({
+app.use(
+  bodyParser.json({
     limit: "30mb",
-    extended: true
-}));
-app.use(bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
     limit: "30mb",
-    extended: true
-}))
-app.use(cors())
+    extended: true,
+  })
+);
+app.use(cors());
+app.use("/posts", postRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port:${PORT}`)))
-    .catch((error) => console.log(error.message));
+mongoose
+  .connect("mongodb://127.0.0.1:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port:${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
